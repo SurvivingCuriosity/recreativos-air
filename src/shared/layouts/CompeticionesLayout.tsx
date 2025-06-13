17c1194ba8d3df76887c80bb4bf7ede9f892532d
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-06-13 13:39:33 +0200

    Update CompeticionesLayout.tsx

--- src/shared/layouts/CompeticionesLayout.tsx
+++ src/shared/layouts/CompeticionesLayout.tsx
@@ -12,7 +12,7 @@ export const CompeticionesLayout = () => {
     <div className="bg-neutral-950 text-neutral-50 min-h-svh">
       <TopNav content={TopNavContent}/>
       <div className="pt-14 h-full relative max-w-screen-lg mx-auto">
-        <img src='logo.png' alt='logo' className='w-full opacity-[4%] pointer-events-none fixed -bottom-1/12 left-1/3' />
+        <img src='/logo.png' alt='logo' className='w-full opacity-[4%] pointer-events-none fixed -bottom-1/12 left-1/3' />
         <Outlet />
       </div>
     </div>
