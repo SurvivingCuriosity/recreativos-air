4a90a27b7dc5cfd4bf2da3b2895f14f5d70c641b
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-10-21 17:40:45 +0200

    Prod url

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { getAccessToken } from "./auth/authStorage";
 
+const BASE_URL =
+  import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL // Vercel (producción)
+    : "http://192.168.0.19:3000/api"; // desarrollo local
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://192.168.0.19:3000/api",
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
